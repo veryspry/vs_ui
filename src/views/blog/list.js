4c4b4e9ee9e24843388470cf29bcf2ebce2088c7
veryspry <ehlinger.matt@gmail.com>
2019-02-01 17:59:17 -0600

    update loading screen

--- src/views/blog/list.js
+++ src/views/blog/list.js
@@ -52,7 +52,6 @@ const BlogList = props => {
         const {
           listSummaries: { items }
         } = data;
-        return <Loading />;
         return (
           <Flex justifyContent="spaceBetween" alignItems="center" py="60px">
             <Flex
